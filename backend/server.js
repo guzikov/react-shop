241b3bc2cbd4c558b6ba04e99616af2c07c4ce3d
Sergei Guzikov <sguzikov@gmail.com>
2021-06-12 14:49:52 +0300

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,17 @@ app.use('/api/upload', uploadRoutes)
 
 app.use('/uploads', express.static(path.join(__dirname, '/../uploads')))
 
+if (process.env.APP_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/../frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 //Get PAYPAL config
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
